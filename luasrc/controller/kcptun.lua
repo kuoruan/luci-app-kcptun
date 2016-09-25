0f6e38f62e65532c6645ae903144dcc79ae0703e
kuoruan <kuoruan@gmail.com>
2016-09-25 21:00:14 +0800

    日志支持实时刷新

--- luasrc/controller/kcptun.lua
+++ luasrc/controller/kcptun.lua
@@ -30,53 +30,59 @@ function index()
     entry({"admin", "services", "kcptun", "server"},
         cbi("kcptun/server")).leaf = true
 
-    entry({"admin", "services", "kcptun", "status"}, call("status"))
+    entry({"admin", "services", "kcptun", "info"}, call("kcptun_info"))
 end
 
-function status()
+function kcptun_info()
     local sys  = require "luci.sys"
     local uci  = require "luci.model.uci".cursor()
     local http = require "luci.http"
     local fs = require "nixio.fs"
+    local kcptun = "kcptun"
 
-    local status = {
+    local enable_server = uci:get_first(kcptun, "general", "enable_server") == "1"
+    local enable_logging = uci:get_first(kcptun, "general", "enable_logging") == "1"
+    local log_folder = uci:get_first(kcptun, "general", "log_folder") or ""
+
+    local info = {
         client = {
-            version = 0,
-            running = false
-        },
-        server = {
-            enabled = (uci:get_first("kcptun", "general", "enable_server") == "1"),
-            version = 0,
             running = false
         }
     }
 
-    local client_file = uci:get_first("kcptun", "general", "client_file") or ""
-    if client_file:trim() ~= "" then
-        if fs.access(client_file, "rwx", "rx", "rx") then
-            status.client.version = tostring(sys.exec(client_file .. " -v | awk '{printf $3}'") or 0)
-        end
-    end
-
     local client_pid = tonumber(fs.readfile("/var/run/kcptun/client.pid") or 0)
     if client_pid > 0 then
-        status.client.running = (sys.call("ps | awk '{print $1}' | grep -q " .. client_pid) == 0)
+        info.client.running = (sys.call("ps | awk '{print $1}' | grep -q " .. client_pid) == 0)
     end
 
-    if status.server.enabled then
-        local server_file = uci:get_first("kcptun", "general", "server_file") or ""
-        if server_file:trim() ~= "" then
-            if fs.access(server_file, "rwx", "rx", "rx") then
-                status.server.version = tostring(sys.exec(server_file .. " -v | awk '{printf $3}'") or 0)
-            end
+    if enable_logging and log_folder ~= "" then
+        local client_log_file = log_folder .. "/kcptun-client.log"
+        if fs.access(client_log_file) then
+            info.client.log = sys.exec("tail -n 20 " .. client_log_file)
+        else
+            info.client.log = "error"
         end
+    end
 
+    if enable_server then
+        info.server = {
+            running = false
+        }
         local server_pid = tonumber(fs.readfile("/var/run/kcptun/server.pid") or 0)
         if server_pid > 0 then
-            status.server.running = (sys.call("ps | awk '{print $1}' | grep -q " .. server_pid) == 0)
+            info.server.running = (sys.call("ps | awk '{print $1}' | grep -q " .. server_pid) == 0)
+        end
+
+        if enable_logging and log_folder ~= "" then
+            local server_log_file = log_folder .. "/kcptun-server.log"
+            if fs.access(server_log_file) then
+                info.server.log = sys.exec("tail -n 20 " .. server_log_file)
+            else
+                info.server.log = "error"
+            end
         end
     end
 
     http.prepare_content("application/json")
-    http.write_json(status)
+    http.write_json(info)
 end
